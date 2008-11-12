3ab3a069ec3dfd98c44edc615893712ea2fbb7b7
Crak <crak.otaku@gmail.com>
2008-11-12 14:09:00 +0000

    None

--- proof_of_concept/megaupload_parsers.py
+++ proof_of_concept/megaupload_parsers.py
@@ -1,4 +1,5 @@
-#######################################################################################	Tucan Project
+###############################################################################
+##	Tucan Project
 
 ##	Copyright (C) 2008 Fran Lupion Crakotak(at)yahoo.es
 ##	Copyright (C) 2008 Paco Salido beakman(at)riseup.net
@@ -17,8 +18,7 @@
 ##	You should have received a copy of the GNU General Public License
 ##	along with this program; if not, write to the Free Software
 ##	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#####################################################################################
-
+###############################################################################
 
 import urllib2
 import math
