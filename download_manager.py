89046bce10e764c1312230dadf2da45005a1dec2
Crak <crak.otaku@gmail.com>
2008-12-08 22:03:50 +0000

    Detectado el limite de descargas

--- download_manager.py
+++ download_manager.py
@@ -135,7 +135,7 @@ class DownloadManager:
 				print download.name, status, progress, actual_size, unit, speed, time
 				if status:
 					download.update(status, progress, actual_size, unit, speed, time)
-					if status in [cons.STATUS_PEND, cons.STATUS_STOP, cons.STATUS_ERROR]
+					if status in [cons.STATUS_PEND, cons.STATUS_STOP, cons.STATUS_ERROR]:
 						link.active = False
 						download.progress = 0
 						link.plugin.return_download_slot()
