6062c77e5eb6c9a942cc40f28b965118e87ba615
Crak <crak.otaku@gmail.com>
2008-12-10 19:11:53 +0000

    Statusbar ya muestra el estado actual

--- downloader.py
+++ downloader.py
@@ -80,7 +80,7 @@ class Downloader(threading.Thread):
 					else:
 						self.status = cons.STATUS_ERROR
 			except urllib2.HTTPError, e:
-				print "exception", e
+				print "[%4d-%02d-%02d %02d:%02d:%02d] %s" % (lt[0], lt[1], lt[2], lt[3], lt[4], lt[5], e)
 				self.stop_flag = True
 				self.status = cons.STATUS_PEND
 
