4bd145a44f9887d90b9e21397c9006c950ebd2da
Crak <crak.otaku@gmail.com>
2008-12-16 22:02:27 +0000

    ventana de preferencias

--- preferences.py
+++ preferences.py
@@ -35,17 +35,12 @@ class Preferences(gtk.Dialog):
 		self.set_title("Preferences")
 		self.set_size_request(500,500)
 		
-		notebook = gtk.Notebook()
-		notebook.set_property("homogeneous", True)
-		self.vbox.pack_start(notebook)
-		for item in [cons.ICON_PREFERENCES_MAIN, cons.ICON_PREFERENCES_PLUGINS, cons.ICON_PREFERENCES_ADVANCED]:
-			vbox = gtk.VBox()
-			vbox.pack_start(gtk.image_new_from_file(item))
-			vbox.pack_start(gtk.Label(item))
-			vbox.show_all()
-			page = gtk.VBox()
-			notebook.append_page(page, vbox)
-			notebook.set_tab_label_packing(page, True, True, gtk.PACK_START)
+		self.notebook = gtk.Notebook()
+		self.notebook.set_property("homogeneous", True)
+		self.vbox.pack_start(self.notebook)
+		self.new_page("General Configuration", cons.ICON_PREFERENCES_MAIN, self.init_main())
+		self.new_page("Service Configuration", cons.ICON_PREFERENCES_SERVICES, self.init_services())
+		self.new_page("Advanced Configuration", cons.ICON_PREFERENCES_ADVANCED, self.init_advanced())
 
 		#action area
 		cancel_button = gtk.Button(None, gtk.STOCK_CANCEL)
@@ -58,7 +53,67 @@ class Preferences(gtk.Dialog):
 		self.connect("response", self.close)
 		self.show_all()
 		self.run()
+		
+	def new_page(self, tab_name, tab_image, page):
+		""""""
+		vbox = gtk.VBox()
+		vbox.pack_start(gtk.image_new_from_file(tab_image))
+		vbox.pack_start(gtk.Label(tab_name))
+		vbox.show_all()
+		self.notebook.append_page(page, vbox)
+		self.notebook.set_tab_label_packing(page, True, True, gtk.PACK_START)
+		
+	def init_main(self):
+		""""""
+		vbox = gtk.VBox()
+		
+		frame = gtk.Frame()
+		frame.set_label_widget(gtk.image_new_from_file(cons.ICON_NETWORK))
+		vbox.pack_start(frame)
+		vbox1 = gtk.VBox()
+		frame.add(vbox1)
+		
+		hbox = gtk.HBox()
+		label = gtk.Label("Max simultaneous downloads: ")
+		hbox.pack_start(label, False, False, 10)
+		aspect = gtk.AspectFrame()
+		hbox.pack_start(aspect)
+		spinbutton = gtk.SpinButton()
+		spinbutton.set_range(1,10)
+		spinbutton.set_increments(1,0)
+		spinbutton.set_numeric(True)
+		hbox.pack_start(spinbutton, False, False, 10)
+		vbox1.pack_start(hbox, False, False, 5)
+		
+		hbox = gtk.HBox()
+		label = gtk.Label("Max simultaneous uploads: ")
+		hbox.pack_start(label, False, False, 10)
+		aspect = gtk.AspectFrame()
+		hbox.pack_start(aspect)
+		spinbutton = gtk.SpinButton()
+		spinbutton.set_range(1,10)
+		spinbutton.set_increments(1,0)
+		spinbutton.set_numeric(True)
+		hbox.pack_start(spinbutton, False, False, 10)
+		vbox1.pack_start(hbox, False, False, 5)
+		
+		frame = gtk.Frame()
+		frame.set_label_widget(gtk.image_new_from_file(cons.ICON_FOLDER))
+		vbox.pack_start(frame)
+		frame = gtk.Frame()
+		frame.set_label_widget(gtk.image_new_from_file(cons.ICON_LANGUAGE))
+		vbox.pack_start(frame)
+		vbox.show_all()
+		return vbox
 
+	def init_services(self):
+		""""""
+		return gtk.VBox()
+		
+	def init_advanced(self):
+		""""""
+		return gtk.VBox()
+		
 	def close(self, widget=None, other=None):
 		""""""
 		self.destroy()
