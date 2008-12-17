7c20fb807413b1d72307862ba7a2f2f091e31b29
Crak <crak.otaku@gmail.com>
2008-12-17 23:11:08 +0000

    prueba de concepto de gigasize

--- proof_of_concept/gigasize_captcha.py
+++ proof_of_concept/gigasize_captcha.py
@@ -22,16 +22,16 @@
 
 import urllib
 import urllib2
-
-[('method', 'post'), ('action', '/formdownload.php'), ('target', '_top')]
-[('name', 'txtNumber'), ('type', 'text'), ('id', 'txtNumber'), ('value', ''), ('maxlength', '3'), ('size', '5'), ('class', 'inpcode')]
-[('name', 'btnLogin'), ('type', 'image'), ('id', 'btnLogin'), ('src', '/images/premium-banners//download_button.png'), ('value', 'Download')]
-
-
-
+import cookielib
 
 if __name__ == "__main__":
+	urllib2.install_opener(urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar())))
+	urllib2.urlopen(urllib2.Request("http://www.gigasize.com/get.php/3196987695/p3x03sp.avi"))
 	f = open("tmp.jpg", "w")
 	f.write(urllib2.urlopen(urllib2.Request("http://www.gigasize.com/randomImage.php")).read())
 	f.close()
-	print urllib2.urlopen(urllib2.Request("http://www.gigasize.com/get.php/3196987695/p3x03sp.avi")).read()
+	captcha = str(raw_input()).strip()
+	data = urllib.urlencode({"txtNumber": captcha, "btnLogin.x": "124", "btnLogin.y": "12", "btnLogin": "Download"})
+	handle = urllib2.urlopen(urllib2.Request("http://www.gigasize.com/formdownload.php"), data)
+	print handle.read()
+	handle.close()
