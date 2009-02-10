71ca8f9cfeab1f1967ce54ded4149e9113678fc7
Crak <crak.otaku@gmail.com>
2009-02-10 18:41:06 +0000

    notificacion de limites de descarga

--- statusbar.py
+++ statusbar.py
@@ -20,25 +20,23 @@
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ###############################################################################
 
+import time
+
 import pygtk
 pygtk.require('2.0')
 import gtk
 
-from custom_window import FeatherWindow
+from feather_window import FeatherWindow
 
-class LimitStack(gtk.Window):
+class Statusbar(gtk.Statusbar):
 	""""""
 	def __init__(self, widget=None):
 		""""""
-		gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
-		self.set_position(gtk.WIN_POS_CENTER)
-		self.set_size_request(800, -1)
-		self.set_decorated(False)
-		statusbar = gtk.Statusbar()
-		statusbar.set_has_resize_grip(False)
-		self.add(statusbar)
+		gtk.Statusbar.__init__(self)
+		self.set_has_resize_grip(False)
 		
-		self.stack = [(None, "AnonymousRapidshare", "[10:32]"), (None, "AnonymousMegaupload", "[09:19]")]
+		self.stack = {}
+		self.menu = gtk.Menu()
 		
 		label = gtk.Label("Limits: ")
 		statusbar.pack_start(label, False)
@@ -51,19 +49,28 @@ class LimitStack(gtk.Window):
 		
 		button.connect("clicked", self.show_stack)
 		self.show_all()
-				
+		
+	def add_limit(self, plugin, icon):
+		""""""
+		now = time.localtime()
+		limit = gtk.MenuItem()
+		self.stack[plugin] = limit
+		limit.add(gtk.image_new_from_stock(gtk.STOCK_INFO, gtk.ICON_SIZE_MENU))
+		limit.connect("enter-notify-event", self.feather.show_feather, plugin, "[" + str(now[3]) + ":" + str(now[4]) + "]")
+		limit.connect("leave-notify-event", self.feather.hide_feather)
+		limit.connect("activate", self.feather.hide_feather)
+		self.menu.append(limit)
+		
+	def remove_limit(self, plugin):
+		""""""
+		if plugin in self.stack.keys():
+			self.menu.remove(self.stack[plugin])
+			del self.stack[plugin]
+		
 	def show_stack(self, widget):
 		""""""
-		menu = gtk.Menu()
-		for icon, name, time in self.stack:
-			tmp = gtk.MenuItem()
-			tmp.add(gtk.image_new_from_stock(gtk.STOCK_INFO, gtk.ICON_SIZE_MENU))
-			tmp.connect("enter-notify-event", self.feather.show_feather, name, time)
-			tmp.connect("leave-notify-event", self.feather.hide_feather)
-			tmp.connect("activate", self.feather.hide_feather)
-			menu.append(tmp)
-		menu.show_all()
-		menu.popup(None, None, self.menu_position, 1, 0, widget.get_allocation())
+		self.menu.show_all()
+		self.menu.popup(None, None, self.menu_position, 1, 0, widget.get_allocation())
 		
 	def menu_position(self, menu, rect):
 		""""""
@@ -73,4 +80,8 @@ class LimitStack(gtk.Window):
 
 if __name__ == "__main__":
 	g = LimitStack()
+	import gobject
+	for plugin, icon in [("AnonymousRapidshare", None), ("AnonymousMegaupload", None)]:
+		g.add_limit(plugin, icon)
+	gobject.timeout_add(5000, g.remove_limit, "AnonymousRapidshare")
 	gtk.main()
