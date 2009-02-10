45dd9c38db70d0f31697319eaebc1b187eeab4f5
Crak <crak.otaku@gmail.com>
2009-02-10 17:38:16 +0000

    prototipo del sistema de notificacion de limites

--- proof_of_concept/custom_window.py
+++ proof_of_concept/custom_window.py
@@ -1,4 +1,24 @@
-import math
+###############################################################################
+## Tucan Project
+##
+## Copyright (C) 2008-2009 Fran Lupion crakotaku(at)yahoo.es
+## Copyright (C) 2008-2009 Paco Salido beakman(at)riseup.net
+## Copyright (C) 2008-2009 JM Cordero betic0(at)gmail.com
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+###############################################################################
 
 import pygtk
 pygtk.require('2.0')
@@ -93,7 +113,9 @@ FEATHER = ["255 82 2 1",
 "                                                                                                                                                                     .....                                                                                     "]
 
 class FeatherWindow(gtk.Window):
+	""""""
 	def __init__(self, parent):
+		""""""
 		gtk.Window.__init__(self, gtk.WINDOW_POPUP)
 		self.set_transient_for(parent)
 
@@ -110,6 +132,7 @@ class FeatherWindow(gtk.Window):
 		self.connect('size-allocate', self._on_size_allocate)
 
 	def _on_size_allocate(self, window, allocation):
+		""""""
 		pixmap, bitmap = gtk.gdk.pixmap_create_from_xpm_d(self.get_root_window(), None, FEATHER)
 		width, height = pixmap.get_size()
 		self.set_size_request(width, height)
