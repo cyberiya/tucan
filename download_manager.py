ef17d638e8173420006a2990b1f6b3c7f6ddcbe4
Crak <crak.otaku@gmail.com>
2009-03-09 23:21:31 +0000

    posible bug con mediafire y los limites

--- download_manager.py
+++ download_manager.py
@@ -79,8 +79,9 @@ class DownloadManager:
 		result = [] 
 		for service in self.services:
 			if service.anonymous_download_plugin:
-				if (("limit" in dir(service.anonymous_download_plugin)) and (service.anonymous_download_plugin.limit)):
-					result.append((service.name, cons.TYPE_ANONYMOUS, "[%s]" % time.strftime("%H:%M"), service.icon_path))
+				if "limit" in dir(service.anonymous_download_plugin):
+					if service.anonymous_download_plugin.limit)):
+						result.append((service.name, cons.TYPE_ANONYMOUS, "[%s]" % time.strftime("%H:%M"), service.icon_path))
 		return result
 		
 	def delete_link(self, name, link):
