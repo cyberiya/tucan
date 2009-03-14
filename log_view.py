8868ce79d772a74280d1fd1665e51847bc677be1
Crak <crak.otaku@gmail.com>
2009-03-14 18:05:34 +0000

    ventana de logs

--- log_view.py
+++ log_view.py
@@ -20,13 +20,14 @@
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ###############################################################################
 
-import time
-
 import pygtk
 pygtk.require('2.0')
 import gtk
 import gobject
 
+import cons
+
+SEVERITY = ["1", "2", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL", "#"]
 
 class LogView(gtk.Dialog):
 	""""""
@@ -36,7 +37,11 @@ class LogView(gtk.Dialog):
 		self.set_title("Log View")
 		self.set_size_request(700,500)
 		self.set_icon(self.render_icon(gtk.STOCK_FILE, gtk.ICON_SIZE_MENU))
-		
+
+		self.file = open("/home/crak/user-guide-index.txt", "r")
+		self.back_buffer = gtk.TextBuffer()
+		self.back_buffer.set_text(self.file.read())
+
 		#textview
 		frame = gtk.Frame()
 		self.vbox.pack_start(frame)
@@ -51,32 +56,62 @@ class LogView(gtk.Dialog):
 		scroll.get_vadjustment().connect("value-changed", self.value_changed)		
 		
 		scroll.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
-		buffer = gtk.TextBuffer()		
-		self.textview = gtk.TextView(buffer)
+		self.textview = gtk.TextView(gtk.TextBuffer())
 		scroll.add(self.textview)
 		self.textview.set_wrap_mode(gtk.WRAP_CHAR)
 		self.textview.set_editable(False)
 		
-		self.file = open("/home/crak/user-guide-index.txt", "r")
-		buffer.insert_at_cursor(self.file.read())
-
-		#button = gtk.Button(None, gtk.STOCK_REFRESH)
-		#self.action_area.pack_start(button)
-		#button.connect("clicked", self.new_captcha)
-		#button = gtk.Button(None, gtk.STOCK_ADD)
-		#self.action_area.pack_start(button)
-		#button.connect("clicked", self.store_captcha)
+		#combo
+		hbox = gtk.HBox()
+		self.vbox.pack_start(hbox, False, False, 10)
+		buttonbox = gtk.HButtonBox()
+		hbox.pack_start(buttonbox, False, False, 10)
+		label = gtk.Label("Minimum severity shown.")
+		hbox.pack_start(label, False, False, 10)
+		aspect = gtk.AspectFrame()
+		aspect.set_shadow_type(gtk.SHADOW_NONE)
+		hbox.pack_start(aspect)
+		
+		self.combo = gtk.combo_box_new_text()
+		buttonbox.pack_start(self.combo)
+		self.combo.connect("changed", self.reload)
+		
+		for s in SEVERITY:
+			self.combo.append_text(s)
+		self.combo.set_active(2)
+		
+		#action area
+		button = gtk.Button(None, gtk.STOCK_CLOSE)
+		self.action_area.pack_start(button)
+		button.connect("clicked", self.close)
 
 		self.connect("response", self.close)
 		self.show_all()
 		
 		gobject.timeout_add(1000, self.update)
 		self.run()
-		
+	
+	def reload(self, textview):
+		""""""
+		buffer = self.textview.get_buffer()
+		ini, fin = self.back_buffer.get_bounds()
+		for line in self.back_buffer.get_text(ini, fin).split("\n"):
+			for s in SEVERITY[self.combo.get_active():]:
+				if s in line:
+					buffer.insert(buffer.get_end_iter(), line + "\n")
+					break
+			print line
+	
 	def update(self):
 		""""""
 		try:
-			self.textview.get_buffer().insert_at_cursor(self.file.readline())
+			line = self.file.readline()
+			self.back_buffer.insert(self.back_buffer.get_end_iter(), line)
+			for s in SEVERITY[self.combo.get_active():]:
+				if s in line:
+					buffer = self.textview.get_buffer()
+					buffer.insert(buffer.get_end_iter(), line)
+					break
 		except:
 			pass
 		else:
