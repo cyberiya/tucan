5097f00b6de0b6ed15684bab9a2296dfaa5d42ce
Crak <crak.otaku@gmail.com>
2009-03-18 22:48:53 +0000

    version 0.3.6

--- cons.py
+++ cons.py
@@ -27,7 +27,7 @@ import logging
 
 # project constants
 TUCAN_NAME = "Tucan Manager"
-TUCAN_VERSION = "0.3.6 alpha"
+TUCAN_VERSION = "0.3.5 alpha"
 WEBPAGE = "http://cusl3-tucan.forja.rediris.es/"
 DOC = "http://cusl3-tucan.forja.rediris.es/doc/"
 
