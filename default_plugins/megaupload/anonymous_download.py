fb6921b77d07344cd1c1aa64fe1acd2a8efbe9b5
Crak <crak.otaku@gmail.com>
2009-03-18 23:37:17 +0000

    solucionado problema con la base de datos

--- default_plugins/megaupload/anonymous_download.py
+++ default_plugins/megaupload/anonymous_download.py
@@ -81,7 +81,7 @@ class AnonymousDownload(DownloadPlugin, Slots):
 		""""""
 		if "win" in sys.platform:
 			try:
-				subprocess.call([os.path.join(sys.path[0], "captcha.exe"), url, "check"], creationflags=134217728)
+				logger.warning(subprocess.call([os.path.join(sys.path[0], "captcha.exe"), url, "check"], creationflags=134217728))
 				f = open(os.path.join(cons.PLUGIN_PATH, "megaupload", "check.dat"), "rb")
 				data = pickle.loads(f.read())
 				f.close()
