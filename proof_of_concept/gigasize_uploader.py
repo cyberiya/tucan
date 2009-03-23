8c04fc74256ea9eee4867be0df8e913d289ec0df
Crak <crak.otaku@gmail.com>
2009-03-23 14:11:13 +0000

    sistema de subidas

--- proof_of_concept/gigasize_uploader.py
+++ proof_of_concept/gigasize_uploader.py
@@ -38,36 +38,29 @@ class UploadParser(HTMLParser):
 		self.action = None
 		self.id = None
 		cookie = cookielib.CookieJar()
-		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+		simple_opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
 
 		req = urllib2.Request("http://www.gigasize.com/")
-		handle = opener.open(req)
-		print handle.info()
+		handle = simple_opener.open(req)
+
 		self.feed(handle.read())
 		self.close()
 		if self.action:
-			form = {"filecontent": open(file_name, "rb"), "u.x": "51", "u.y": "5"}
+			print simple_opener.open(urllib2.Request("http://www.gigasize.com/upload_progress.php", urllib.urlencode([("sid", self.id), ("_", "")]), HEADER)).read()
+
 			opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie), MultipartHTTPHandler())
-			
-			req = urllib2.Request(self.action, form, HEADER)
-			handle = opener.open(req)
-			print handle.info()
-			mierda = open("/home/crak/mierda.html", "w")
-			for line in  handle.readlines():
-				mierda.write(line)
-				if "Error" in line:
-					print line
-			mierda.close()
+			form = [("UPLOAD_IDENTIFIER", self.id), ("file_1", None), ("file_0", open(file_name, "rb")), ("titlel", ""), ("description", ""), ("numupfiles", ""), ("x", 78), ("y", 5), ("accept", 1)]
+			handle = opener.open(urllib2.Request("http://www.gigasize.com%s" % self.action, form, HEADER))
+			print simple_opener.open(urllib2.Request("http://www.gigasize.com/upload_progress.php", urllib.urlencode([("sid", self.id), ("_", "")]), HEADER)).read()
 
 	def handle_starttag(self, tag, attrs):
 		""""""
 		if tag == "form":
-			print tag, attrs
-			if attrs[3][1] == "multipart/form-data":
-				#self.action = attrs[2][1]
-				pass
+			if attrs[0][1] == "multipart/form-data":
+				self.action = attrs[1][1]
 		elif tag == "input":
-			print tag, attrs
+			if attrs[1][1] == "UPLOAD_IDENTIFIER":
+				self.id = attrs[2][1]
 
 if __name__ == "__main__":
-	c = UploadParser("/home/crak/2009-03-03-210652_1024x600_scrot.png", "mierda")
+	c = UploadParser("/home/crak/2009-03-03-210729_1024x600_scrot.png", "mierda")
