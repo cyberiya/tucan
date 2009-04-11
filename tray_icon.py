acba421166075ca30ebc03194d19251283a94a47
Crak <crak.otaku@gmail.com>
2009-04-11 00:07:39 +0000

    ya no se muestra informacion sobre la revision

--- tray_icon.py
+++ tray_icon.py
@@ -31,7 +31,7 @@ class TrayIcon(gtk.StatusIcon):
 	def __init__(self, show, hide, menu):
 		""""""
 		gtk.StatusIcon.__init__(self)
-		self.set_tooltip("Tucan Manager - Version: " + cons.TUCAN_VERSION + cons.REVISION)
+		self.set_tooltip("Tucan Manager - Version: %s" % cons.TUCAN_VERSION)
 		self.set_from_file(cons.ICON_TUCAN)
 		self.set_visible(True)
 		
