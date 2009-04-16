a0856431872820467b6d1ce11718c7c170c35f68
Crak <crak.otaku@gmail.com>
2009-04-16 00:18:46 +0000

    icono de proceso al comenzar con el captcha evitando parecer bloqueado

--- download_manager.py
+++ download_manager.py
@@ -130,6 +130,7 @@ class DownloadManager:
 		""""""
 		for download in self.pending_downloads:
 			if name == download.name:
+				download.status = cons.STATUS_WAIT
 				for link in download.links:
 					link.plugin, link.type = self.get_plugin(link.service)
 					if link.plugin.add(download.path, link.url, download.name):
