76e6c278db85b10abc40903ad4ecb8270a59c617
Crak <crak.otaku@gmail.com>
2009-12-23 22:46:34 +0000

    actualizacion de easy-share

--- default_plugins/easy-share/anonymous_download.py
+++ default_plugins/easy-share/anonymous_download.py
@@ -30,7 +30,7 @@ class AnonymousDownload(DownloadPlugin, Slots):
 	""""""
 	def __init__(self):
 		""""""
-		Slots.__init__(self, 1, 15)
+		Slots.__init__(self, 1, 300)
 		DownloadPlugin.__init__(self)
 
 	def check_links(self, url):
