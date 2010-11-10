f4f527b9587189c82e73f919fe47a2ac9fdb51d8
Elie M?lois <eliemelois@gmail.com>
2010-11-10 07:12:38 +0000

    Indicator-applet support. Need localization and a bit of testing

--- reviews-0.3.10/ui/gtk/tray_icon.py
+++ reviews-0.3.10/ui/gtk/tray_icon.py
@@ -59,52 +59,91 @@ if cons.OS_OSX:
 				""""""
 				pass
 
-class TrayIcon(gtk.StatusIcon):
-	""""""
-	def __init__(self, show, hide, menu):
+try:
+	import appindicator
+except Exception:
+	class TrayIcon(gtk.StatusIcon):
 		""""""
-		gtk.StatusIcon.__init__(self)
-		self.set_tooltip("%s - %s" % (cons.TUCAN_NAME, cons.TUCAN_VERSION))
-		self.set_from_file(media.ICON_TUCAN)
-		self.set_visible(True)
+		def __init__(self, show, hide, menu):
+			""""""
+			gtk.StatusIcon.__init__(self)
+			self.set_tooltip("%s - %s" % (cons.TUCAN_NAME, cons.TUCAN_VERSION))
+			self.set_from_file(media.ICON_TUCAN)
+			self.set_visible(True)
 
-		self.window_visible = True
-		self.show_window = show
-		self.hide_window = hide
-
-		self.menu = gtk.Menu()
-		for item in menu:
-			if item == None:
-				tmp = gtk.SeparatorMenuItem()
-			else:
-				tmp = gtk.ImageMenuItem(item[0])
-				tmp.connect('activate', item[1])
-			self.menu.append(tmp)
-		self.menu.show_all()
-
-		self.connect('activate', self.activate)
-		self.connect('popup-menu', self.popup_menu)
-
-	def activate(self, statusicon, event=None):
-		""""""
-		if self.window_visible:
-			self.hide_window()
-			self.window_visible = False
-		else:
-			self.show_window()
 			self.window_visible = True
+			self.show_window = show
+			self.hide_window = hide
 
-	def popup_menu(self, statusicon, button, time):
-		""""""
-		self.menu.popup(None, None, None, button, time, self)
+			self.menu = gtk.Menu()
+			for item in menu:
+				if item == None:
+					tmp = gtk.SeparatorMenuItem()
+				else:
+					tmp = gtk.ImageMenuItem(item[0])
+					tmp.connect('activate', item[1])
+				self.menu.append(tmp)
+			self.menu.show_all()
 
-	def change_tooltip(self, statusbar, context, text):
-		""""""
-		#if context == "Downloads":
-		tmp = text.split("\t")
-		message = "Downloads: " + "".join(tmp[1:]) + "\n" + tmp[0].strip()
-		self.set_tooltip(message)
+			self.connect('activate', self.activate)
+			self.connect('popup-menu', self.popup_menu)
 
-	def close(self):
-		""""""
-		self.set_visible(False)
+		def activate(self, statusicon, event=None):
+			""""""
+			if self.window_visible:
+				self.hide_window()
+				self.window_visible = False
+			else:
+				self.show_window()
+				self.window_visible = True
+
+		def popup_menu(self, statusicon, button, time):
+			""""""
+			self.menu.popup(None, None, None, button, time, self)
+
+		def change_tooltip(self, statusbar, context, text):
+			""""""
+			#if context == "Downloads":
+			tmp = text.split("\t")
+			message = "Downloads: " + "".join(tmp[1:]) + "\n" + tmp[0].strip()
+			self.set_tooltip(message)
+
+		def close(self):
+			""""""
+			self.set_visible(False)
+else:
+	class TrayIcon(appindicator.Indicator):
+			""""""
+			def __init__(self, show, hide, menu):
+				""""""
+				appindicator.Indicator.__init__(self,'Tucan',media.ICON_TUCAN,appindicator.CATEGORY_APPLICATION_STATUS);
+				self.set_status(appindicator.STATUS_ACTIVE)
+
+				self.window_visible = True
+				self.show_window = show
+				self.hide_window = hide
+				
+				self.menu = gtk.Menu()
+				
+				tmp = gtk.MenuItem("Show Tucan")
+				tmp.connect('activate',self.show)
+				self.menu.append(tmp)
+				tmp = gtk.SeparatorMenuItem()
+				self.menu.append(tmp)
+				
+				for item in menu:
+					if item == None:
+						tmp = gtk.SeparatorMenuItem()
+					else:
+						tmp = gtk.ImageMenuItem(item[0])
+						tmp.connect('activate', item[1])
+					self.menu.append(tmp)
+				self.menu.show_all()
+				self.set_menu(self.menu)
+				
+			def show(self,arg):
+				self.show_window()
+
+			def close(self):
+				""""""
+				pass
