a6b58380e330a885ad2c5d97b00b2b49f9ee1234
Elie M?lois <eliemelois@gmail.com>
2010-11-27 01:12:32 +0000

    QueueModel

--- reviews-0.3.10/proof_of_concept/upload/queue_model.py
+++ reviews-0.3.10/proof_of_concept/upload/queue_model.py
@@ -19,115 +19,151 @@
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ###############################################################################
 
+import time
 import logging
 logger = logging.getLogger(__name__)
 
 import pygtk
 pygtk.require('2.0')
 import gtk
+import gobject
+
+from queue import Queue, Package, File, Link
 
 import cons
-from queue import *
 
-class QueueModel(gtk.GenericTreeModel):
-	column_types = (gtk.gdk.Pixbuf, str, int, int, int, int)
-
-	def __init__(self, queue):
+class QueueModel(gtk.GenericTreeModel, Queue):
+	""""""
+	def __init__(self):
+		""""""
 		gtk.GenericTreeModel.__init__(self)
-		self.queue = queue
-		self.items = queue.items
-		return
+		Queue.__init__(self)
+		self.column_types = (gtk.gdk.Pixbuf, str, int, int, int, int)
 		
-	def get_index(self, node):
-		packages = self.queue.get_children()
-		length = 0
-		for i in range(0, node[0]):
-			length += self.queue.get_length(packages[i].id)
-		if len(node) > 1:
-			length += 1
-			files = self.queue.get_children(packages[node[0]].id)
-			for i in range(0, node[1]):
-				length += self.queue.get_length(files[i].id)
-		if len(node) > 2:
-			length += 1
-			length += node[2]
-		return length
-
-	def get_column_names(self):
-		return self.column_names[:]
+	def set_value(self, id, key, value):
+		""""""
+		path = self.on_get_path(id)
+		self.row_changed(path, self.get_iter(path))
+		Queue.set_value(self, id, key, value)
 
 	def on_get_flags(self):
-		return 0
+		""""""
+		return gtk.TREE_MODEL_ITERS_PERSIST
 
 	def on_get_n_columns(self):
+		""""""
 		return len(self.column_types)
 
-	def on_get_column_type(self, n):
-		return self.column_types[n]
+	def on_get_column_type(self, num):
+		""""""
+		if num < len(self.column_types):
+			return self.column_types[num]
 
 	def on_get_iter(self, path):
-		return path
+		""""""
+		print path
+		if path:
+			packages = self.get_children()
+			if path[0] < len(packages):
+				package = packages[path[0]]
+				if len(path) > 1:
+					files = self.get_children(package.id)
+					if path[1] < len(files):
+						file = files[path[1]]
+						if len(path) > 2:
+							links = self.get_children(file.id)
+							if path[2] < len(links):
+								return links[path[2]].id
+						else:
+							return file.id
+				else:
+					return package.id
 
-	def on_get_path(self, node):
-		return self.get_index(node)
+	def on_get_path(self, iter_id):
+		""""""
+		if iter_id:
+			package_cont = -1
+			file_cont = -1
+			link_cont = -1
+			for item in self.items:
+				if isinstance(item, Package):
+					package_cont += 1
+					if item.id == iter_id:
+						return (package_cont, )
+					else:
+						file_cont = -1
+						link_cont = -1
+				elif isinstance(item, File):
+					file_cont += 1
+					if item.id == iter_id:
+						return (package_cont, file_cont)
+					else:
+						link_cont = -1
+				elif isinstance(item, Link):
+					link_cont += 1
+					if item.id == iter_id:
+						return (package_cont, file_cont, link_cont)
 
-	def on_get_value(self, node, column):
+	def on_get_value(self, iter_id, column):
+		""""""
+		item = self.get_item(iter_id)
 		if column is 0:
 			return None
 		if column is 1:
-			if isinstance(self.items[self.get_index(node)], Link):
-				return self.items[self.get_index(node)].plugin
+			if isinstance(item, Link):
+				return item.plugin
 			else:
-				return self.items[self.get_index(node)].name
+				return item.name
 		elif column is 2:
-			return self.items[self.get_index(node)].get_progress()
+			return item.get_progress()
 		elif column is 3:
-			return self.items[self.get_index(node)].current_size
+			return item.current_size
 		elif column is 4:
-			return self.items[self.get_index(node)].total_size
+			return item.total_size
 		elif column is 5:
-			return self.items[self.get_index(node)].current_speed
+			return item.current_speed
 
-	def on_iter_next(self, node):
-		try:
-			if node != None:
-				item = self.items[self.get_index(node)]
-				if node[-1] == len(self.queue.get_children(item.parent_id))-1: # last node at level
-					return None
-				return node[:-1] +(node[-1]+1,)
-		except IndexError:
-			return None
+	def on_iter_next(self, iter_id):
+		""""""
+		item = self.get_item(iter_id)
+		if item:
+			items = self.get_children(item.parent_id)
+			ind = items.index(item)
+			if ind+1 < len(items):
+				return items[ind+1].id
 
-	def on_iter_children(self, node):
-		if node == None: # top of tree
-			return(0,)
-		if len(node) >= 3: # no more levels
-			return None
-		return node +(0,)
+	def on_iter_children(self, iter_id):
+		""""""
+		items = self.get_children(iter_id)
+		if items:
+			return items[0].id
 
-	def on_iter_has_child(self, node):
-		item = self.items[self.get_index(node)]
-		return node == None or not isinstance(item, Link)
+	def on_iter_has_child(self, iter_id):
+		""""""
+		items = self.get_children(iter_id)
+		if items:
+			return True
 
-	def on_iter_n_children(self, node):
-		if node:
-			item = self.items[self.get_index(node)]
-			return len(self.queue.get_children(item.id))
-		return len(self.queue.get_children())
-
-	def on_iter_nth_child(self, node, n):
-		if node:
-			return node +(0,)
+	def on_iter_n_children(self, iter_id):
+		""""""
+		items = self.get_children(iter_id)
+		if items:
+			return len(items)
 		else:
-			return (n,)
+			return 0
 
-	def on_iter_parent(self, child):
-		assert child != None
-		if child:
-			return child[:-1]
-		else:
-			return None
+	def on_iter_nth_child(self, iter_id, n):
+		""""""
+		assert n >= 0
+		items = self.get_children(iter_id)
+		if n < len(items):
+			return items[n].id
 
+	def on_iter_parent(self, iter_id):
+		""""""
+		item = self.get_item(iter_id)
+		if item:
+			return item.parent_id
 
 class GenericTreeModelExample:
 	def delete_event(self, widget, event, data=None):
@@ -145,7 +181,7 @@ class GenericTreeModelExample:
 		PATH1 = '/home/user/file.part1.rar'
 		PATH2 = '/home/user/file.part2.rar'
 		SIZE = 1024
-		SIZE2 = 25
+		SIZE2 = 2048
 		LINK1 = 'megaupload.anonymous_upload'
 		LINK2 = 'rapidshare.anonymous_upload'
 
@@ -160,15 +196,11 @@ class GenericTreeModelExample:
 		
 		FILE_LIST3 = [
 		(PATH2, SIZE, [LINK1, LINK2, LINK1])]
-		
-		queue = Queue()
-		queue.add_package(FILE_LIST)
-		queue.add_package(FILE_LIST2)
-		queue.add_package(FILE_LIST3)
-		queue.items[0].current_size = 1245
- 
- 
-		self.listmodel = QueueModel(queue)
+
+		self.listmodel = QueueModel()
+		self.listmodel.add_package(FILE_LIST)
+		self.listmodel.add_package(FILE_LIST2)
+		self.listmodel.add_package(FILE_LIST3)
  
 		# create the TreeView
 		self.treeview = gtk.TreeView()
@@ -197,8 +229,15 @@ class GenericTreeModelExample:
 		self.window.add(self.scrolledwindow)
 		self.treeview.set_model(self.listmodel)
 		self.window.show_all()
-
+		
+	def update(self):
+		""""""
+		for item in self.listmodel.items:
+			self.listmodel.set_value(item.id, "current_size", item.current_size+10)
+		if item.current_size != item.total_size:
+			return True
 
 if __name__ == "__main__":
-	gtmexample = GenericTreeModelExample()
+	g = GenericTreeModelExample()
+	gobject.timeout_add(1000, g.update)
 	gtk.main()
