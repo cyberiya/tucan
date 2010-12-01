79256e41d45a9ed2b6ce3319969d8f3d67548017
Crak <crak.otaku@gmail.com>
2010-12-01 22:37:12 +0000

    falling back to png support

--- reviews-0.3.10/ui/gtk/media.py
+++ reviews-0.3.10/ui/gtk/media.py
@@ -3,7 +3,7 @@
 ##
 ## Copyright (C) 2008-2010 Fran Lupion crak@tucaneando.com
 ##
-## This program is free software; you can redistribute it and/or modify
+## This program is free software; you can redistribute it andor modify
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation; either version 3 of the License, or
 ## (at your option) any later version.
@@ -30,32 +30,32 @@ from core.cons import PATH
 
 try:
 	EXT = ".svg"
-	PATH_MEDIA = os.path.join(PATH, "media", "scalable")
-	gtk.gdk.pixbuf_new_from_file("%s/tucan%s" % (PATH_MEDIA, EXT))
+	PATH_MEDIA = os.path.join(PATH, "media", "scalable", "")
+	gtk.gdk.pixbuf_new_from_file("%stucan%s" % (PATH_MEDIA, EXT))
 except Exception, e:
 	EXT = ".png"
-	PATH_MEDIA = os.path.join(PATH, "media")
+	PATH_MEDIA = os.path.join(PATH, "media", "")
 	logger.debug("Using PNG icons: %s" % e)
 
-ICON_TUCAN = "%s/tucan%s" % (PATH_MEDIA, EXT)
-ICON_DOWNLOAD = "%s/document-save%s" % (PATH_MEDIA, EXT)
-ICON_UPLOAD = "%s/system-software-update%s" % (PATH_MEDIA, EXT)
-ICON_CLEAR = "%s/edit-delete%s" % (PATH_MEDIA, EXT)
-ICON_DOWN = "%s/go-down%s" % (PATH_MEDIA, EXT)
-ICON_UP = "%s/go-up%s" % (PATH_MEDIA, EXT)
-ICON_START = "%s/media-playback-start%s" % (PATH_MEDIA, EXT)
-ICON_STOP = "%s/media-playback-stop%s" % (PATH_MEDIA, EXT)
-ICON_CHECK = "%s/software-update-available%s" % (PATH_MEDIA, EXT)
-ICON_PACKAGE = "%s/package-x-generic%s" % (PATH_MEDIA, EXT)
-ICON_PREFERENCES = "%s/preferences-system%s" % (PATH_MEDIA, EXT)
-ICON_PREFERENCES_MAIN = "%s/preferences-desktop%s" % (PATH_MEDIA, EXT)
-ICON_PREFERENCES_SERVICES = "%s/contact-new%s" % (PATH_MEDIA, EXT)
-ICON_PREFERENCES_ADVANCED = "%s/applications-system%s" % (PATH_MEDIA, EXT)
-ICON_LANGUAGE = "%s/preferences-desktop-locale%s" % (PATH_MEDIA, EXT)
-ICON_FOLDER = "%s/user-home%s" % (PATH_MEDIA, EXT)
-ICON_NETWORK = "%s/network-error%s" % (PATH_MEDIA, EXT)
-ICON_ADVANCED = "%s/application-x-executable%s" % (PATH_MEDIA, EXT)
-ICON_MISSING = "%s/image-missing%s" % (PATH_MEDIA, EXT)
-ICON_ACCOUNT = "%s/system-users%s" % (PATH_MEDIA, EXT)
-ICON_UPDATE = "%s/software-update-urgent%s" % (PATH_MEDIA, EXT)
-ICON_SEND = "%s/mail-reply-sender%s" % (PATH_MEDIA, EXT)
+ICON_TUCAN = "%stucan%s" % (PATH_MEDIA, EXT)
+ICON_DOWNLOAD = "%sdocument-save%s" % (PATH_MEDIA, EXT)
+ICON_UPLOAD = "%ssystem-software-update%s" % (PATH_MEDIA, EXT)
+ICON_CLEAR = "%sedit-delete%s" % (PATH_MEDIA, EXT)
+ICON_DOWN = "%sgo-down%s" % (PATH_MEDIA, EXT)
+ICON_UP = "%sgo-up%s" % (PATH_MEDIA, EXT)
+ICON_START = "%smedia-playback-start%s" % (PATH_MEDIA, EXT)
+ICON_STOP = "%smedia-playback-stop%s" % (PATH_MEDIA, EXT)
+ICON_CHECK = "%ssoftware-update-available%s" % (PATH_MEDIA, EXT)
+ICON_PACKAGE = "%spackage-x-generic%s" % (PATH_MEDIA, EXT)
+ICON_PREFERENCES = "%spreferences-system%s" % (PATH_MEDIA, EXT)
+ICON_PREFERENCES_MAIN = "%spreferences-desktop%s" % (PATH_MEDIA, EXT)
+ICON_PREFERENCES_SERVICES = "%scontact-new%s" % (PATH_MEDIA, EXT)
+ICON_PREFERENCES_ADVANCED = "%sapplications-system%s" % (PATH_MEDIA, EXT)
+ICON_LANGUAGE = "%spreferences-desktop-locale%s" % (PATH_MEDIA, EXT)
+ICON_FOLDER = "%suser-home%s" % (PATH_MEDIA, EXT)
+ICON_NETWORK = "%snetwork-error%s" % (PATH_MEDIA, EXT)
+ICON_ADVANCED = "%sapplication-x-executable%s" % (PATH_MEDIA, EXT)
+ICON_MISSING = "%simage-missing%s" % (PATH_MEDIA, EXT)
+ICON_ACCOUNT = "%ssystem-users%s" % (PATH_MEDIA, EXT)
+ICON_UPDATE = "%ssoftware-update-urgent%s" % (PATH_MEDIA, EXT)
+ICON_SEND = "%smail-reply-sender%s" % (PATH_MEDIA, EXT)
