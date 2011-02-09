ca1f751ad670911defb080c8b61940e0d9b81271
Elie M?lois <eliemelois@gmail.com>
2011-02-09 21:30:10 +0100

    Mediafire fix

--- src/tests/_default_plugins/test_mediafire.py
+++ src/tests/_default_plugins/test_mediafire.py
@@ -34,7 +34,7 @@ CONF_PATH = "../default_plugins/mediafire"
 SERVICE_NAME = "mediafire"
 
 TEST_INVALID_LINK = "http://www.mediafire.com/?00000"
-TEST_LINK = "http://www.mediafire.com/?4aakein8a55miv3"
+TEST_LINK = "http://www.mediafire.com/?8r3cgzxoza0b5bq"
 TEST_SIZE = 114
 TEST_UNIT = "KB"
 
