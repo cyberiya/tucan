2434e37ec8aeb58ce7cf14afe94c60880f307b8c
Ali Shah <ahshah@airpost.net>
2011-02-11 00:30:42 -0500

    Updated check_links functionality in anonymous_download.py Removed unneccesary testing in premium_cookie.py

--- src/default_plugins/hotfile/anonymous_download.py
+++ src/default_plugins/hotfile/anonymous_download.py
@@ -31,25 +31,47 @@ BASE_URL = "http://hotfile.com"
 
 def check_links(url):
 	""""""
-	name = None
-	size = -1
-	unit = None
-	try:
-		for line in URLOpen().open(url):
-			if 'class="arrow_down"' in line:
-				tmp = line.split("</strong>")
-				name = tmp[1].split("<span>")[0].strip()
-		tmp = tmp[1].split("<strong>")[1].split(" ")
-		unit = tmp[1].upper()
-		size = float(tmp[0])
-		if unit == "MB" and not int(size):
-		  size = int(size*1024)
-		  if not size:
-			  size = 1
-			  unit = "KB"
+	if url is None:
+		return None
+
+	name   = None
+	size   = -1
+	unit   = None
+	status = -1
+
+	
+	"""
+	Split the string by '/':
+	Hotfile urls are always of this form:
+		http://hotfile.com/dl/ID/KEY/filename.html
+	Thus we should get the (0 based) 4th & 5th entry in the returned list
+	"""
+	split_str = url.split('/')
+	if len(split_str) is not 7:
+		return None
+
+	link_id  = split_str[4]
+	link_key = split_str[5]
+	del split_str
+	check_link_url = ("http://api.hotfile.com/?action=checklinks&ids=" + link_id + 
+					  "&keys=" + link_key + "&fields=name,size,status")
+	""" print ("Check link url: {0}".format(check_link_url))  """
+
+	try: 
+		link_name_size_status = URLOpen().open(check_link_url).readline()
+		link_name_size_status_list = link_name_size_status.split(',')
+		name   = link_name_size_status_list[0]
+		size   = int(link_name_size_status_list[1])
+		status = int(link_name_size_status_list[2])
+		unit = "B"
 	except Exception, e:
 		logger.exception("%s :%s" % (url, e))
-	return name, size, unit
+		
+	if status != 1:
+		""" print ("Link is down! {0} {1}|".format(type(status), status)) """
+		return None, -1, None
+	else:
+		return name, size, unit
 
 class AnonymousDownload(DownloadPlugin):
 	""""""
