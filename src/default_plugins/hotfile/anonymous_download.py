fa51cfe133b313fb1a2b30bf843ed9e030bf8bd4
Ali Shah <ahshah@airpost.net>
2011-02-12 16:50:55 -0500

    Cleaning up hotfile check_links function. Remaned to check_link_validity and placed in check_link_validity.py module

--- src/default_plugins/hotfile/anonymous_download.py
+++ src/default_plugins/hotfile/anonymous_download.py
@@ -27,6 +27,7 @@ from core.download_plugin import DownloadPlugin
 from core.recaptcha import Recaptcha
 from core.url_open import URLOpen
 
+from link_validity_check import link_validity_check
 BASE_URL = "http://hotfile.com"
 
 
@@ -119,46 +120,4 @@ class AnonymousDownload(DownloadPlugin):
 		return link, form, wait
 
 	def check_links(self, url):
-		""""""
-		if url is None:
-			return None
-
-		name   = None
-		size   = -1
-		unit   = None
-		status = -1
-
-		
-		"""
-		Split the string by '/':
-		Hotfile urls are always of this form:
-			http://hotfile.com/dl/ID/KEY/filename.html
-		Thus we should get the (0 based) 4th & 5th entry in the returned list
-		"""
-		split_str = url.split('/')
-		if len(split_str) is not 7:
-			return None
-
-		link_id  = split_str[4]
-		link_key = split_str[5]
-		del split_str
-		check_link_url = ("http://api.hotfile.com/?action=checklinks&ids=" + link_id + 
-						  "&keys=" + link_key + "&fields=name,size,status")
-		""" print ("Check link url: {0}".format(check_link_url))  """
-
-		try: 
-			link_name_size_status = URLOpen().open(check_link_url).readline()
-			link_name_size_status_list = link_name_size_status.split(',')
-			name   = link_name_size_status_list[0]
-			size   = int(link_name_size_status_list[1]) / 1024
-			status = int(link_name_size_status_list[2])
-			unit = "KB"
-		except Exception, e:
-			logger.exception("%s :%s" % (url, e))
-			
-		if status != 1:
-			""" print ("Link is down! {0} {1}|".format(type(status), status)) """
-			return None, -1, None
-		else:
-			return name, size, unit
-
+		return link_validity_check(url)
