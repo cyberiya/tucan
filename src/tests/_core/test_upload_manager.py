be139f01b7a0613acaed84b825b536cb0748956e
Crak <crak.otaku@gmail.com>
2011-03-02 23:12:59 +0100

    start/stop implemented in upload_manager

--- src/tests/_core/test_upload_manager.py
+++ src/tests/_core/test_upload_manager.py
@@ -21,32 +21,92 @@
 import time
 import unittest
 
+import core.cons as cons
 from core.queue import Queue
 from core.upload_manager import UploadManager
+
+PACKAGE = [('/home/user/file.part1.rar', 1024, ['megaupload.anonymous_upload', 'rapidshare.anonymous_upload']), ('/home/user/file.part2.rar', 1024, ['megaupload.anonymous_upload', 'rapidshare.anonymous_upload'])]
 
 class TestUploadManager(unittest.TestCase):
 	""""""
 	def setUp(self):
 		""""""
 		self.manager = UploadManager(Queue())
+		#disable scheduler
 		self.manager.scheduling = True
-		self.manager.add([('/home/user/file.part1.rar', 1024, ['megaupload.anonymous_upload'])])
+		self.manager.add(PACKAGE)
 
 	def test_start(self):
-		link = self.manager.queue.items[2]
-		self.manager.start(2, link)
-		while self.manager.get_active_threads():
-			pass
+		""""""
+		package = self.manager.queue.get_children()[0]
+		file1, file2 = self.manager.queue.get_children(package.id)
+		link1, link2 = self.manager.queue.get_children(file1.id)
+		link3, link4 = self.manager.queue.get_children(file2.id)
+		self.manager.start(file1.id)
+		self.manager.start(link3.id)
+		self.assertEqual(package.status, cons.STATUS_ACTIVE)
+		self.assertEqual(file1.status, cons.STATUS_ACTIVE)
+		self.assertEqual(link1.status, cons.STATUS_ACTIVE)
+		self.assertEqual(link2.status, cons.STATUS_ACTIVE)
+		self.assertEqual(link3.status, cons.STATUS_ACTIVE)
+		time.sleep(1)
+		self.assertEqual(package.status, cons.STATUS_PEND)
+		self.assertEqual(file1.status, cons.STATUS_CORRECT)
+		self.assertEqual(link1.status, cons.STATUS_CORRECT)
+		self.assertEqual(link2.status, cons.STATUS_CORRECT)
+		self.assertEqual(link3.status, cons.STATUS_CORRECT)
+		self.manager.start(file2.id)
+		time.sleep(1)
+		self.assertEqual(package.status, cons.STATUS_CORRECT)
+		self.assertEqual(file2.status, cons.STATUS_CORRECT)
+		self.assertEqual(link4.status, cons.STATUS_CORRECT)
 	
 	def test_stop(self):
-		link = self.manager.queue.items[2]
-		self.manager.start(2, link)
-		time.sleep(2)
-		self.manager.stop(2, link)
-		while self.manager.get_active_threads():
-			pass
+		""""""
+		package = self.manager.queue.get_children()[0]
+		file1, file2 = self.manager.queue.get_children(package.id)
+		link1, link2 = self.manager.queue.get_children(file1.id)
+		link3, link4 = self.manager.queue.get_children(file2.id)
+		self.manager.start(file1.id)
+		self.manager.stop(link1.id)
+		self.assertEqual(package.status, cons.STATUS_ACTIVE)
+		self.assertEqual(file1.status, cons.STATUS_ACTIVE)
+		self.assertEqual(link1.status, cons.STATUS_STOP)
+		self.assertEqual(link2.status, cons.STATUS_ACTIVE)
+		time.sleep(1)
+		self.manager.start(file1.id)
+		self.manager.stop(file1.id)
+		self.assertEqual(package.status, cons.STATUS_PEND)
+		self.assertEqual(file1.status, cons.STATUS_STOP)
+		self.assertEqual(link1.status, cons.STATUS_STOP)
+		self.assertEqual(link2.status, cons.STATUS_CORRECT)
+		time.sleep(1)
+		self.manager.start(file2.id)
+		self.manager.stop(package.id)
+		self.assertEqual(package.status, cons.STATUS_STOP)
+		self.assertEqual(file1.status, cons.STATUS_STOP)
+		self.assertEqual(file2.status, cons.STATUS_STOP)
+		self.assertEqual(link1.status, cons.STATUS_STOP)
+		self.assertEqual(link2.status, cons.STATUS_CORRECT)
+		self.assertEqual(link3.status, cons.STATUS_STOP)
+		self.assertEqual(link4.status, cons.STATUS_STOP)
+		package.status = cons.STATUS_PEND
+		file1.status = cons.STATUS_PEND
+		link1.status = cons.STATUS_PEND
+		link2.status = cons.STATUS_PEND
+		self.manager.start(link1.id)
+		self.manager.stop(package.id, package, False)
+		self.assertEqual(package.status, cons.STATUS_ACTIVE)
+		self.assertEqual(file1.status, cons.STATUS_ACTIVE)
+		self.assertEqual(link1.status, cons.STATUS_ACTIVE)
+		self.assertEqual(link2.status, cons.STATUS_STOP)
+		time.sleep(1)
+		self.assertEqual(package.status, cons.STATUS_STOP)
+		self.assertEqual(file1.status, cons.STATUS_STOP)
+		self.assertEqual(link1.status, cons.STATUS_CORRECT)
 
 	def clear(self):
+		""""""
 		pass
 
 	def tearDown(self):
