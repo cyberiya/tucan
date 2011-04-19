02e0231bbff4894059f51c73df62e3497f287592
ashah <ashah@airpost.net>
2011-04-19 11:02:32 -0400

    Missing import line for logger in fileserve check_links.py module

--- src/default_plugins/fileserve/check_links.py
+++ src/default_plugins/fileserve/check_links.py
@@ -18,6 +18,9 @@
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ###############################################################################
 
+import logging
+logger = logging.getLogger(__name__)
+
 from core.url_open import URLOpen
 
 class CheckLinks():
